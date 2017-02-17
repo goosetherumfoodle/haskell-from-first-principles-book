becb5b48324705b2cdfcb9f6a9fdcb90d0ee3b76
goosetherumfoodle <goosetherumfoodle@gmail.com>
2017-02-17 00:11:36 -0500

    23 and more 22

---
+++ 23.hs
@@ -0,0 +1,96 @@
+-- needed for non-state solution
+import System.Random
+-- needed for state-based solution
+import Control.Applicative (liftA3)
+import Control.Monad (replicateM)
+import Control.Monad.Trans.State
+import Data.Monoid ((<>))
+
+-- begin non-state version
+data Die = SideOne
+         | SideTwo
+         | SideThree
+         | SideFour
+         | SideFive
+         | SideSix
+         deriving (Eq, Show)
+
+intToDie :: Int -> Die
+intToDie n = case n of
+               1 -> SideOne
+               2 -> SideTwo
+               3 -> SideThree
+               4 -> SideFour
+               5 -> SideFive
+               6 -> SideSix
+               -- don't use `error` in real code
+               x -> error $ "intToDie got non 1-6 integer: " ++ show x
+
+rollDieThreeTimes :: Int -> (Die, Die, Die)
+rollDieThreeTimes seed = do
+  let s = mkStdGen seed
+      (d1, s1) = randomR (1, 6) s
+      (d2, s2) = randomR (1, 6) s1
+      (d3, _)  = randomR (1, 6) s2
+  (intToDie d1, intToDie d2, intToDie d3)
+-- end non-state version
+
+-- begin state version
+rollDie :: State StdGen Die
+rollDie = state $ do
+  (n, s) <- randomR (1, 6)
+  return (intToDie n, s)
+
+rollDie' :: State StdGen Die
+rollDie' = intToDie <$> state (randomR (1, 6))
+
+rollDieThreeTimes' :: State StdGen (Die, Die, Die)
+rollDieThreeTimes' = liftA3 (,,) rollDie rollDie rollDie
+
+-- pg 870
+
+-- repeats a single die value
+infiniteDie :: State StdGen [Die]
+infiniteDie = repeat <$> rollDie
+
+-- replicateM :: Applicative m => Int -> m a -> m [a]
+
+nDie :: Int -> State StdGen [Die]
+nDie n = replicateM n rollDie
+
+-- pg 871
+
+rollsToGetTwenty :: StdGen -> Int
+rollsToGetTwenty g = go 0 0 g where
+  go :: Int -> Int -> StdGen -> Int
+  go sum count gen
+    | sum >= 20 = count
+    | otherwise =
+    let (die, nextGen) = randomR (1, 6) gen
+    in go (sum + die) (count + 1) nextGen
+
+-- ex Roll Your Own pg 872
+
+-- 1
+
+rollsToGetN :: Int -> StdGen -> Int
+rollsToGetN target gen = go 0 0 gen where
+  go :: Int -> Int -> StdGen -> Int
+  go sum count gen
+    | sum >= target = count
+    | otherwise =
+    let (die, nextGen) = randomR (1, 6) gen
+    in go (sum + die) (count + 1) nextGen
+
+-- 2
+
+rollsCountLogged :: Int -> StdGen -> (Int, [Die])
+rollsCountLogged target g = go 0 0 g []  where
+  go :: Int -> Int -> StdGen -> [Die] -> (Int, [Die])
+  go sum count gen dice
+    | sum >= target = (count, dice)
+    | otherwise =
+    let (die, nextGen) = randomR (1, 6) gen
+    in go (sum + die) (count + 1) nextGen (dice <> [intToDie die])
+
+main = (rollsToGetTwenty . mkStdGen) <$> randomIO
--- chp22/chp22.hs
+++ chp22/chp22.hs
@@ -1,4 +1,7 @@
+module ReaderPractice where
+
 import Control.Applicative (liftA2)
+import Data.Maybe
 
 newtype Reader r a = Reader { runReader :: r -> a }
 
@@ -74,10 +77,80 @@ getDogM = do
 getDogM' :: Person -> Dog
 getDogM' = dogName >>= (\name -> address >>= (\addy -> return $ Dog name addy))
 
-
--- Hint: constrast the type with the Applicative instance and perform the most obvious change you can imagine to make it work
 instance Monad (Reader r) where
   return = pure
 
 -- (>>=) :: Reader r a -> (a -> Reader r b) -> Reader r b
   (Reader ra) >>= aRb = Reader $ (runReader . aRb . ra) <*> id
+
+fli :: a -> b -> c -> a
+fli f a b = f
+
+-- how does that type signature become this one?
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f a b = f b a
+
+-- chpt Exercises pg 856
+
+x = [1, 2, 3]
+y = [4, 5, 6]
+z = [7, 8, 9]
+
+-- lookup :: Eq a => a -> [(a, b)] -> Maybe b
+
+xs :: Maybe Integer
+xs = lookup 3 $ zip x y
+
+ys :: Maybe Integer
+ys = lookup 6 $ zip y z
+
+zs :: Maybe Integer
+zs = lookup 4 $ zip x y
+
+z' :: Integer -> Maybe Integer
+z' n = lookup n $ zip x z
+
+x1 :: Maybe (Integer, Integer)
+x1 = liftA2 (,) xs ys
+
+x2 :: Maybe (Integer, Integer)
+x2 = liftA2 (,) ys zs
+
+x3 :: Integer -> (Maybe Integer, Maybe Integer)
+x3 = liftA2 (,) z' z'
+
+-- pg 858
+
+-- uncurry :: (a -> b -> c) -> (a, b) -> c
+
+summed :: Num c => (c, c) -> c
+summed = uncurry (+)
+
+bolt :: Integer -> Bool
+bolt = liftA2 (&&) (>3) (<8)
+
+-- pg 859
+
+sequA :: Integral a => a -> [Bool]
+sequA = sequenceA [(>3), (<8), even]
+
+s' :: Maybe Integer
+s' = summed <$> ((,) <$> xs <*> ys)
+
+main :: IO ()
+main = do
+  print $ foldr (&&) False $ sequA 3
+  print $ sequA $ fromMaybe 0 s'
+  print $ bolt $ fromMaybe 0 ys
+
+
+  -- do
+  -- print $ sequenceA [Just 3, Just 2, Just 1]
+  -- print $ sequenceA [x, y]
+  -- print $ sequenceA [xs, ys]
+  -- print $ summed <$> ((,) <$> xs <*> ys)
+  -- print $ fmap summed ((,) <$> xs <*> zs)
+  -- print $ bolt 7
+  -- print $ fmap bolt z
+  -- print $ sequenceA [(>3), (<8), even] 7
