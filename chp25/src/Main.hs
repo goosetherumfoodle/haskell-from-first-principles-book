1b83fef7f0ca55082f232b6bad85f2bd0d6f1cdc
goosetherumfoodle <goosetherumfoodle@gmail.com>
2017-04-17 17:36:58 -0400

    adds bifunctor instances

--- chp25/src/Main.hs
+++ chp25/src/Main.hs
@@ -51,6 +51,65 @@ instance (Foldable f, Foldable g) => Foldable (Compose f g) where
 instance (Traversable f, Traversable g, Applicative f) => Traversable (Compose f g) where
    traverse f comp = sequenceA $ f <$> comp
 
+
+-- where does bifunctor get the expectation that p will be of kind * -> * -> *?
+
+class Bifunctor p where
+  {-# MINIMAL bimap | first, second #-}
+
+  bimap :: (a -> b) -> (c -> d) -> p a c -> p b d
+  bimap f g = first f . second g
+
+  first :: (a -> b) -> p a c -> p b c
+  first f = bimap f id
+
+  second :: (b -> c) -> p a b -> p a c
+  second = bimap id
+
+-- 1.
+data Deux a b = Deux a b
+
+instance Bifunctor Deux where
+  bimap f g (Deux a b) = Deux (f a) (g b)
+
+-- 2.
+data Const a b = Const a
+
+instance Bifunctor Const where
+  bimap f _ (Const a) = Const (f a)
+
+-- 3.
+data Drei a b c = Drei a b c
+
+instance Bifunctor (Drei a)  where
+  bimap f g (Drei a b c) = Drei a (f b) (g c)
+
+-- 4.
+data SuperDrei a b c = SuperDrei a b
+
+instance Bifunctor (SuperDrei a) where
+  bimap f _ (SuperDrei a b) = SuperDrei a (f b)
+
+-- 5.
+data SemiDrei a b c = SemiDrei a
+
+instance Bifunctor (SemiDrei a) where
+  bimap f g (SemiDrei a) = SemiDrei a
+
+-- 6.
+data Quadriceps a b c d = Quadzzz a b c d
+
+instance Bifunctor (Quadriceps a b) where
+  bimap f g (Quadzzz a b c d) = Quadzzz a b (f c) (g d)
+
+-- 7.
+-- data Either a b = Left a | Right b
+-- (already defined...)
+
+instance Bifunctor Either where
+  bimap f _ (Left a) = Left $ f a
+  bimap _ g (Right b) = Right $ g b
+
 main :: IO ()
 main = do
   putStrLn "hello world"
