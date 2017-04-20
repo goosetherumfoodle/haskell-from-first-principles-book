816a10f0bbab9fcb79e9e55f9f28a2843d2e565f
goosetherumfoodle <goosetherumfoodle@gmail.com>
2017-04-19 23:22:49 -0400

    finishes chpt25

--- chp25/src/Main.hs
+++ chp25/src/Main.hs
@@ -110,6 +110,48 @@ instance Bifunctor Either where
   bimap f _ (Left a) = Left $ f a
   bimap _ g (Right b) = Right $ g b
 
+-- Plain old Identity. 'a' can be something with
+-- more structure, but it's not required and
+-- Identity won't know anything about it.
+newtype Identity a = Identity { runIdentity :: a } deriving (Eq, Show)
+
+-- The identity monad transformer, serving only to
+-- to specify that additional structure should exist.
+newtype IdentityT f a = IdentityT { runIdentityT :: f a } deriving (Eq, Show)
+
+-- Functor
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity $ f a
+
+instance Functor m => Functor (IdentityT m) where
+  fmap f (IdentityT fa) = IdentityT $ fmap f fa
+
+-- Applicative
+
+instance Applicative Identity where
+  pure = Identity
+
+  (Identity f) <*> (Identity a) = Identity (f a)
+
+instance Applicative m => Applicative (IdentityT m) where
+  pure x = IdentityT $ pure x
+
+  (IdentityT fab) <*> (IdentityT fa) = IdentityT (fab <*> fa)
+
+-- Monad
+
+instance Monad Identity where
+  return = pure
+
+  (Identity a) >>= f = f a
+
+instance Monad m => Monad (IdentityT m) where
+  return = pure
+
+  (>>=) :: IdentityT m a -> (a -> IdentityT m b) -> IdentityT m b
+  (IdentityT ma) >>= f = IdentityT $ Control.Monad.join $ fmap runIdentityT (fmap f ma)
+
 main :: IO ()
 main = do
   putStrLn "hello world"
